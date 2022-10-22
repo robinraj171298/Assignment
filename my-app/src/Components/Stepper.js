c749d923721b8e6d77c08be8ae0522242f0ef96f
robin raj token <robin.raj@amnhealthcare.com>
2022-10-23 04:32:10 +0530

    Updated stepper

--- my-app/src/Components/Stepper.js
+++ my-app/src/Components/Stepper.js
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
+import './Stepper.css'
 
 const steps = [1,2,3,4];
 
@@ -12,7 +13,7 @@ export const HorizontalLinearStepper = props => {
           
           return (
             <Step key={label}>
-              <StepLabel></StepLabel>
+              <StepLabel class="step" StepIconProps={{ sx: { height: '1.5em', width: '1.5em' } }}></StepLabel>
             </Step>
           );
         })}
